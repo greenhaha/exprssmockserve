9d5b222a92a3cc5ab72b04827c3fdf9ce0c1ae9f
greenhaha <a15114101085@aliyun.com>
2021-07-14 21:39:24 +0800

    add mockserve

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const express = require('express');
 const router = require('./router')
 const app = express();
 
+/**
+ * Mock Server
+ * 此处代码解决卡与问题
+ * */
+const cors = require('cors');
+app.use(cors());
+
+// 方案2
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method' )
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE')
+//     res.header('Allow', 'GET, POST, PATCH, OPTIONS, PUT, DELETE')
+//     next();
+//     });
 
 app.use('/', router)
 
